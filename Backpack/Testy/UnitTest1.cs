f349ab4b8ca956339f931ebfde8a590ab3e85f04
AdriannaBieniek <102544492+AdriannaBieniek@users.noreply.github.com>
2023-03-27 23:25:43 +0200

    Add files via upload

---
+++ Backpack/Testy/UnitTest1.cs
@@ -0,0 +1,48 @@
+﻿namespace Testy;
+using Bieniek_plecak;
+public class Tests
+{
+    [Test]
+    public void poprawna_ilosc_przedmiotow() //tworzymy dobra ilosc przedmiotow
+    {
+        int n = 10;
+        for (int i = 0; i < n; i++)
+        {
+            Backpack backpack = new Backpack(i);
+            backpack.CreateItems(i);
+            Assert.AreEqual(i,backpack.items.GetLength(0));
+        }
+    }
+
+    [Test]
+    public void nic_nie_bierzemy()  //przy udzwigu <=1 nie pakujemy nic do plecaka
+    {
+        int ile_przedmiotow = 5;
+        Backpack backpack = new Backpack(ile_przedmiotow);
+        int[,] pom = backpack.CreateItems(ile_przedmiotow);
+        for (int i = -10; i <= 0; i++)
+        {
+            backpack.TakenItems(ile_przedmiotow,i, pom);
+            Assert.AreEqual(0, backpack.taken_amount);
+
+        }
+    }
+
+    [Test]
+    public void nic_nie_bierzemy2()  //przy udzwigu <=1 nie pakujemy nic do plecaka
+    {
+        int ile_przedmiotow = 5;
+        Backpack backpack = new Backpack(ile_przedmiotow);
+        int[,] pom = backpack.CreateItems(ile_przedmiotow);
+        for (int i = -10; i <= 0; i++) //i to udzwig
+        {
+            backpack.TakenItems(ile_przedmiotow, i, pom);
+            for (int j = 0; j < ile_przedmiotow; j++)
+            {
+                Assert.AreEqual(0, backpack.taken.GetValue(j));
+            }
+            }
+    }
+
+
+}
